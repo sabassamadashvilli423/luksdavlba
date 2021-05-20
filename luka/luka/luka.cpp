2ff0b1d092ce27bb9dccb926d7c191fe1253f20e
sabassamadashvilli423 <81419535+sabassamadashvilli423@users.noreply.github.com>
2021-05-20 18:18:50 +0400

    Add files via upload

---
+++ luka/luka/luka.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string s;
+	s = "https://www.facebook.com/inga.tyiuri";
+	//am linkze gadadi da saocrebas naxav igive davalebis fass
+		cout << s;
+}
